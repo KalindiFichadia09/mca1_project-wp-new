37f81812b45151634ca7b904f3c897b598ebdb12
VibhutiChavda22 <vibhutichavda4@gmail.com>
2024-09-23 18:57:30 +0530

    some changes

--- signin.php
+++ signin.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+<!-- head start -->
+<head> 
+        <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In - Jaysheree Jewels</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
