14464249be7a6dbefef0384387c78d78fdbaf747
kalindi <kfichadiya849@rku.ac.in>
2024-12-14 19:08:04 +0530

    change in con

--- conn.php
+++ conn.php
@@ -1,17 +1,9 @@
 <?php
+// $con = mysqli_connect('localhost', 'root', '', 'online_jewelry_system_db');
 $con = mysqli_connect('sql103.infinityfree.com', 'if0_37911774', 'MNNpBpRJnQoL', 'if0_37911774_online_jewelry_system_db');
 if ($con) {
 	// echo "doneeeeeeeee.............";
 } else {
 	echo "not connected !!!!!!";
 }
-?>
-
-<?php
-// $con = mysqli_connect('localhost', 'root', '', 'online_jewelry_system_db');
-// if ($con) {
-// 	// echo "doneeeeeeeee.............";
-// } else {
-// 	echo "not connected !!!!!!";
-// }
 ?>
